3922ab35ba2e925ecb0934a9f5e8b41bfb9a5813
Arinji2 <arinjaydhar205@gmail.com>
2023-06-07 02:06:08 +0530

    Change Supabase Domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["vsuuouckxzowenhwvdhf.supabase.co", "i.scdn.co"],
+    domains: ["hakvrncbkergyfgdvwcf.supabase.co", "i.scdn.co"],
   },
 };
 
